1a09f0b4facd6d84b5a9e38b5afa0f92aaad8004
Frank Grooten <grooten.f@gmail.com>
2020-09-04 11:58:51 +0200

    Update zebrakeyboard.js

--- www/zebrakeyboard.js
+++ www/zebrakeyboard.js
@@ -3,10 +3,10 @@ function zebraKeyboard() {}
 
 // The function that passes work along to native shells
 // layoutGroupName is a string (default: "Vanboxtel_Null_Keyboard", layout is a string (default: "Null_Keyboard")
-zebraKeyboard.prototype.selectLayout = function(successCallback, errorCallback) {
+zebraKeyboard.prototype.selectLayout = function(successCallback, errorCallback,layoutGroupName,layout) {
   var options = {};
-  options.layout = "Null_Keyboard";
-  options.layoutGroupName = "Vanboxtel_Null_Keyboard";
+  options.layoutGroupName = layoutGroupName;
+  options.layout = layout;
   cordova.exec(successCallback, errorCallback, 'ZebraKeyboard', 'selectLayout', [options]);
 }
 
